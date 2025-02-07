1e4453332c91f5389bf3c68883c94942562dd27c
Luca Marino <luca@MacBook-Pro-di-Luca.local>
2025-02-07 12:47:30 +0100

    Implementazione creazione cliente

--- plugins/cat_inventory_management/plugin.py
+++ plugins/cat_inventory_management/plugin.py
@@ -1,38 +1,50 @@
-from cat.mad_hatter.decorators import tool, hook
-from .utili import connect, get_warehouse, create_product, send_mail, send_telegram_notification, create_supplier
 import pandas as pd
-from pydantic import BaseModel, constr
 from cat.experimental.form import CatForm, CatFormState, form
+from cat.mad_hatter.decorators import hook, tool
+from pydantic import BaseModel, constr
+
+from .utili import (
+    create_customer,
+    create_product,
+    create_supplier,
+    get_warehouse,
+    send_mail,
+    send_telegram_notification,
+)
+
 
 @tool(
     return_direct=True,
     examples=[
         "Qual è lo stato del mio magazzino",
         "Come è la situazione del mio magazzino",
-        "Dammi qualche informazione sul mio magazzino"
-    ]
+        "Dammi qualche informazione sul mio magazzino",
+    ],
 )
 def get_the_warehouse_status(tool_input, cat):
     """Rispondi a "Qual è lo stato del mio magazzino", e domande simili. Input è sempre None.."""
 
     mark, _ = get_warehouse()
-    
+
     output = cat.llm(
         f"""Riscrivi, in modo chiaro per l'utente, applicando una formattazione adeguata e senza usare tabelle, i dati contenuti in questa tabella:
         
         {mark}
         
         Non mostrare quantità riservata e quantità minima
-        """, stream=True
+        """,
+        stream=True,
     )
     output = output.replace("**", "")
-    
 
     return output
 
+
 @tool(
     return_direct=True,
-    examples=["Aggiungi il prodotto con quantità disponibile iniziale e quantità minima di riordino"]
+    examples=[
+        "Aggiungi il prodotto con quantità disponibile iniziale e quantità minima di riordino"
+    ],
 )
 def create_new_product(tool_input, cat):
     """Crea un nuovo prodotto nel magazzino con la quantità specificata e la soglia di riordino.
@@ -43,59 +55,77 @@ def create_new_product(tool_input, cat):
     - Il secondo valore è un numero intero o decimale che rappresenta la quantità iniziale disponibile.
     - Il terzo valore è un numero intero o decimale che indica la quantità minima per il riordino.
 
-"""
+    """
 
     product_name, product_qty, product_min_qty = tool_input.split(",")
-    
+
     product_name = product_name.replace(" ", "")
     product_qty = float(product_qty.replace(" ", ""))
-    product_min_qty = (product_min_qty.replace(" ", ""))
-    product_description = cat.llm(
-        f"Genera una descrizione per il prodotto farmaceutico che si chiama {product_name} formattata in HTML"
-    ).replace("```html", "").replace("```", "")
-    
-    result, link = create_product(product_name, product_qty, product_min_qty, product_description)
+    product_min_qty = product_min_qty.replace(" ", "")
+    product_description = (
+        cat.llm(
+            f"Genera una descrizione per il prodotto farmaceutico che si chiama {product_name} formattata in HTML"
+        )
+        .replace("```html", "")
+        .replace("```", "")
+    )
+
+    result, link = create_product(
+        product_name, product_qty, product_min_qty, product_description
+    )
     if result:
-        output = f"Ho creato il prodotto <a href=\"{link}\" target=\"_blank\"> {product_name}</a>"
+        output = f'Ho creato il prodotto <a href="{link}" target="_blank"> {product_name}</a>'
         return output
     else:
         return "Non ho potuto creare il prodotto"
-    
-@tool(
-    return_direct=True,
-    examples=["Invia una mail al responsabile di magazzino"]
-)
+
+
+@tool(return_direct=True, examples=["Invia una mail al responsabile di magazzino"])
 def send_mail_to_wh_manager(tool_input, cat):
-    """Invia una mail per notificare i prodotti che stanno per finire al responsabile di magazzino
-    """
-    _ , df = get_warehouse()
-    df["Quantità Da Riordinare"] = df["Quantità Disponibile"] - df["Quantità Riservata"] - df["Quantità Minima di Riordino"]
+    """Invia una mail per notificare i prodotti che stanno per finire al responsabile di magazzino"""
+    _, df = get_warehouse()
+    df["Quantità Da Riordinare"] = (
+        df["Quantità Disponibile"]
+        - df["Quantità Riservata"]
+        - df["Quantità Minima di Riordino"]
+    )
     df_qty_da_ordinare = df[df["Quantità Da Riordinare"] < 0]
-    df_qty_da_ordinare["Quantità Da Riordinare"] = df_qty_da_ordinare["Quantità Da Riordinare"] * -1
+    df_qty_da_ordinare["Quantità Da Riordinare"] = (
+        df_qty_da_ordinare["Quantità Da Riordinare"] * -1
+    )
     df_qty_da_ordinare = df_qty_da_ordinare[["Prodotto", "Quantità Da Riordinare"]]
 
     df_qty_da_ordinare_mark = df_qty_da_ordinare.to_markdown(index=False)
-    
-    mail_text = cat.llm(f"""Prepara il testo di una mail che dica al responsabile di ordinare
+
+    mail_text = (
+        cat.llm(f"""Prepara il testo di una mail che dica al responsabile di ordinare
                         i seguenti prodotti della tabella {df_qty_da_ordinare_mark}. 
                         Formatta con HTML la mail ma scrivendo solo il body della mail.
-                        Inserisci come nome del responsabile Luca Marino. Firmati come il tuo AIbot di Fiducia""").replace("```html", "").replace("```", "")
-    
+                        Inserisci come nome del responsabile Luca Marino. Firmati come il tuo AIbot di Fiducia""")
+        .replace("```html", "")
+        .replace("```", "")
+    )
+
     send_mail(mail_text, "Notifica Riordino Prodotti")
-    telegram_text = cat.llm(f"""Prepara il testo di un messaggio telegram che dica al responsabile di ordinare
+    telegram_text = (
+        cat.llm(f"""Prepara il testo di un messaggio telegram che dica al responsabile di ordinare
                         i seguenti prodotti della tabella {df_qty_da_ordinare_mark}. Formatta la tabella con il tag <code>.
-                        Inserisci come nome del responsabile Luca Marino. Firmati come il tuo AIbot di Fiducia""").replace("```html", "").replace("```", "")
+                        Inserisci come nome del responsabile Luca Marino. Firmati come il tuo AIbot di Fiducia""")
+        .replace("```html", "")
+        .replace("```", "")
+    )
     send_telegram_notification(telegram_text)
     return "Mail inviata"
-    
+
 
 class Supplier(BaseModel):
-    name: constr(min_length=1)  
+    name: constr(min_length=1)
     street: str
     city: str
     zip: constr(min_length=4, max_length=10)
     phone: constr(min_length=5, max_length=20)
     email: str
+
 
 @form
 class SupplierForm(CatForm):
@@ -115,13 +145,12 @@ class SupplierForm(CatForm):
         "Ho finito con l'aggiunta di un nuovo fornitore",
         "Ho finito con l'inserimento di un nuovo fornitore",
         "Sì",
-        "Sì puoi inserirlo"
+        "Sì puoi inserirlo",
     ]
-    
+
     ask_confirm = True
 
     def submit(self, form_data):
-        
         result, link = create_supplier(
             supplier_name=form_data["name"],
             supplier_street=form_data["street"],
@@ -130,13 +159,14 @@ class SupplierForm(CatForm):
             supplier_phone=form_data["phone"],
             supplier_email=form_data["email"],
         )
-        
+
         supplier_name = form_data["name"]
         if result:
-            return {"output": f"Ho creato il fornitore <a href=\"{link}\" target=\"_blank\"> {supplier_name}</a>"} 
+            return {
+                "output": f'Ho creato il fornitore <a href="{link}" target="_blank"> {supplier_name}</a>'
+            }
         else:
             return {"output": "Non ho potuto creare il fornitore"}
-            
 
     def message_wait_confirm(self):
         prompt = (
@@ -144,7 +174,85 @@ class SupplierForm(CatForm):
             f"{self._generate_base_message()}\n"
             "Dopo il riassunto dei dettaglio Scrivi qualcosa come, 'I dati sono corretti? Posso inserire il fornitore nel sistema? Rispondi dicendo Si puoi inserirlo'"
         )
-        
+
+        print(self._state)
+        return {"output": f"{self.cat.llm(prompt)}"}
+
+    def message_incomplete(self):
+        prompt = (
+            f"Nel form mancano alcuni dettagli:\n{self._generate_base_message()}\n"
+            """In base a ciò che è ancora necessario,
+            crea un suggerimento per aiutare l'utente a compilare il
+            form di inserimento del fornitore"""
+        )
+        return {"output": f"{self.cat.llm(prompt)}"}
+
+
+class Customer(BaseModel):
+    name: constr(min_length=1)
+    street: str
+    city: str
+    zip: constr(min_length=4, max_length=10)
+    phone: constr(min_length=5, max_length=20)
+    email: str
+    customer_type: str
+
+
+@form
+class CustomerForm(CatForm):
+    description = "Inserimento di un cliente"
+    model_class = Customer
+    start_examples = [
+        "Voglio inserire un nuovo cliente",
+        "Voglio inserire un cliente",
+        "Aiutami ad inserire un cliente",
+        "Aiutami ad inserire un nuovo cliente",
+        "Aggiungiamo un nuovo cliente",
+        "Aggiungiamo un cliente",
+    ]
+    stop_examples = [
+        "Non voglio più inserire il cliente",
+        "Non voglio più inserire il nuovo cliente",
+        "Ho finito con l'aggiunta di un nuovo cliente",
+        "Ho finito con l'inserimento di un nuovo cliente",
+        "Sì",
+        "Sì puoi inserirlo",
+    ]
+
+    ask_confirm = True
+
+    def submit(self, form_data):
+        customer_type = self.cat.llm(f"""Prendi questo valore: '{form_data["customer_type"]}'
+e capisci se si tratta di una persona fisica o di un'azienda.
+Se si tratta di una persona fisica restituisci person altrimenti company.
+
+Rispondi solo person o company
+""")
+        result, link = create_customer(
+            customer_name=form_data["name"],
+            customer_street=form_data["street"],
+            customer_city=form_data["city"],
+            customer_zip=form_data["zip"],
+            customer_phone=form_data["phone"],
+            customer_email=form_data["email"],
+            customer_type=customer_type,
+        )
+
+        customer_name = form_data["name"]
+        if result:
+            return {
+                "output": f'Ho creato il cliente <a href="{link}" target="_blank"> {customer_name}</a>'
+            }
+        else:
+            return {"output": "Non ho potuto creare il cliente"}
+
+    def message_wait_confirm(self):
+        prompt = (
+            "Riassumiamo brevemente i dettagli raccolti:\n"
+            f"{self._generate_base_message()}\n"
+            "Dopo il riassunto dei dettaglio Scrivi qualcosa come, 'I dati sono corretti? Posso inserire il fornitore nel sistema? Rispondi dicendo Si puoi inserirlo'"
+        )
+
         print(self._state)
         return {"output": f"{self.cat.llm(prompt)}"}
 
@@ -153,6 +261,6 @@ class SupplierForm(CatForm):
             f"Nel form mancano alcuni dettagli:\n{self._generate_base_message()}\n"
             """In base a ciò che è ancora necessario,
             crea un suggerimento per aiutare l'utente a compilare il 
-            form di inserimento del fornitore"""
+            form di inserimento del cliente. Digli che il tipo di cliente può essere solo o persona fisica o azienda"""
         )
         return {"output": f"{self.cat.llm(prompt)}"}
--- plugins/cat_inventory_management/utili.py
+++ plugins/cat_inventory_management/utili.py
@@ -7,14 +7,21 @@ import telepot
 
 # from config import config
 
+
 def connect(db):
-    """ Connect to the PostgreSQL database server """
+    """Connect to the PostgreSQL database server"""
     conn = None
     try:
         # read connection parameters
-       
+
         # params = config(section=db)
-        params = {'host': 'host.docker.internal', 'database': 'health1', 'user': 'odoo', 'password': 'gattaccio', 'port': '5433'}
+        params = {
+            "host": "host.docker.internal",
+            "database": "health1",
+            "user": "odoo",
+            "password": "gattaccio",
+            "port": "5433",
+        }
 
         # connect to the PostgreSQL server
         # print('Connecting to the PostgreSQL database...')
@@ -28,51 +35,65 @@ def connect(db):
     #         conn.close()
     #         print('Database connection closed.')
 
+
 def get_warehouse():
-    odoo = odoorpc.ODOO('host.docker.internal', port=8069)  # Cambia host e porta se necessario
+    odoo = odoorpc.ODOO(
+        "host.docker.internal", port=8069
+    )  # Cambia host e porta se necessario
 
     # Autenticazione
-    db = 'health1'
-    username = 'admin'
-    password = 'admin'
+    db = "health1"
+    username = "admin"
+    password = "admin"
     odoo.login(db, username, password)
 
     # Modelli Odoo
-    Product = odoo.env['product.product']
-    StockQuant = odoo.env['stock.quant']
-    StockLocation = odoo.env['stock.location']
-    OrderPoint = odoo.env['stock.warehouse.orderpoint']
+    Product = odoo.env["product.product"]
+    StockQuant = odoo.env["stock.quant"]
+    StockLocation = odoo.env["stock.location"]
+    OrderPoint = odoo.env["stock.warehouse.orderpoint"]
 
     # Recupero prodotti con quantità disponibili a magazzino
-    products = Product.search_read([], ['id', 'name'])
+    products = Product.search_read([], ["id", "name"])
 
     # Lista per raccogliere i dati
     data = []
 
     for product in products:
-        product_id = product['id']
-        product_name = product['name']
-        
+        product_id = product["id"]
+        product_name = product["name"]
+
         # Ottieni le quantità per il prodotto
-        quants = StockQuant.search_read([('product_id', '=', product_id)], ['location_id', 'quantity', 'reserved_quantity'])
-        orderpoint = OrderPoint.search_read([('product_id', '=', product_id)], ['product_min_qty'])
-        min_qty = orderpoint[0]['product_min_qty'] if orderpoint else 0  # Default a 0 se non è impostata
+        quants = StockQuant.search_read(
+            [("product_id", "=", product_id)],
+            ["location_id", "quantity", "reserved_quantity"],
+        )
+        orderpoint = OrderPoint.search_read(
+            [("product_id", "=", product_id)], ["product_min_qty"]
+        )
+        min_qty = (
+            orderpoint[0]["product_min_qty"] if orderpoint else 0
+        )  # Default a 0 se non è impostata
 
         for quant in quants:
-            location_id = quant['location_id'][0]
-            
+            location_id = quant["location_id"][0]
+
             # Ottieni il nome del magazzino dalla location
             location_data = StockLocation.browse(location_id)
-            warehouse_name = location_data.display_name if location_data else "Sconosciuto"
-            
+            warehouse_name = (
+                location_data.display_name if location_data else "Sconosciuto"
+            )
+
             # Aggiungi i dati alla lista
-            data.append({
-                'Prodotto': product_name,
-                # 'Magazzino': warehouse_name,
-                'Quantità Disponibile': quant['quantity'],
-                'Quantità Riservata': quant['reserved_quantity'],
-                'Quantità Minima di Riordino': min_qty
-            })
+            data.append(
+                {
+                    "Prodotto": product_name,
+                    # 'Magazzino': warehouse_name,
+                    "Quantità Disponibile": quant["quantity"],
+                    "Quantità Riservata": quant["reserved_quantity"],
+                    "Quantità Minima di Riordino": min_qty,
+                }
+            )
 
     # Creazione DataFrame
     df = pd.DataFrame(data)
@@ -80,54 +101,65 @@ def get_warehouse():
 
     # Stampa il DataFrame
     mark = df.to_markdown(index=False)
-    
+
     return mark, df
 
 
 def create_product(product_name, product_qty, product_min_qty, product_description):
-    odoo = odoorpc.ODOO('host.docker.internal', port=8069)  
+    odoo = odoorpc.ODOO("host.docker.internal", port=8069)
 
-    db = 'health1'
-    username = 'admin'
-    password = 'admin'
+    db = "health1"
+    username = "admin"
+    password = "admin"
     odoo.login(db, username, password)
 
-    Product = odoo.env['product.product']
-    StockQuant = odoo.env['stock.quant']
-    StockLocation = odoo.env['stock.location']
-    OrderPoint = odoo.env['stock.warehouse.orderpoint']
+    Product = odoo.env["product.product"]
+    StockQuant = odoo.env["stock.quant"]
+    StockLocation = odoo.env["stock.location"]
+    OrderPoint = odoo.env["stock.warehouse.orderpoint"]
 
+    new_product_id = Product.create(
+        {
+            "name": product_name,
+            "is_storable": True,
+            "categ_id": 1,
+            "description": product_description,
+        }
+    )
 
-    new_product_id = Product.create({
-        'name': product_name,
-        'is_storable': True,  
-        'categ_id': 1,
-        'description': product_description,
-    })
+    location_id = StockLocation.search([("usage", "=", "internal")], limit=1)[0]
 
+    StockQuant.create(
+        {
+            "product_id": new_product_id,
+            "location_id": location_id,
+            "quantity": product_qty,
+        }
+    )
 
-    location_id = StockLocation.search([('usage', '=', 'internal')], limit=1)[0]
+    OrderPoint.create(
+        {
+            "product_id": new_product_id,
+            "location_id": location_id,
+            "product_min_qty": product_min_qty,
+            "product_max_qty": product_min_qty,
+        }
+    )
 
-    StockQuant.create({
-        'product_id': new_product_id,
-        'location_id': location_id,
-        'quantity': product_qty,
-    })
-
-    OrderPoint.create({
-        'product_id': new_product_id,
-        'location_id': location_id,
-        'product_min_qty': product_min_qty,  
-        'product_max_qty': product_min_qty, 
-    })
-    
-    odoo_url = 'http://localhost:8069/web#id={}&model=product.product&view_type=form'.format(new_product_id)
+    odoo_url = (
+        "http://localhost:8069/web#id={}&model=product.product&view_type=form".format(
+            new_product_id
+        )
+    )
     return [True, odoo_url]
 
+
 def send_mail(mail_text, mail_sbj):
-# Configura l'API
+    # Configura l'API
     configuration = mailslurp_client.Configuration()
-    configuration.api_key['x-api-key'] = "5dc12d2c8d8db594054b652023c644844d324a8a3d3694c3a34f7a45283c2dec"
+    configuration.api_key["x-api-key"] = (
+        "5dc12d2c8d8db594054b652023c644844d324a8a3d3694c3a34f7a45283c2dec"
+    )
 
     with ApiClient(configuration) as api_client:
         api_instance = mailslurp_client.InboxControllerApi(api_client)
@@ -141,16 +173,18 @@ def send_mail(mail_text, mail_sbj):
             to=["marino.luca07@gmail.com"],
             subject=mail_sbj,
             body=mail_text,
-            is_html=True
+            is_html=True,
         )
 
         api_instance.send_email(inbox.id, send_options)
 
+
 def send_telegram_notification(telegram_text):
-    TOKEN = '7865121599:AAGWZOQ2Cnmpyr7En0PZC5npYLhSpIQgG5Q'
+    TOKEN = "7865121599:AAGWZOQ2Cnmpyr7En0PZC5npYLhSpIQgG5Q"
     bot = telepot.Bot(TOKEN)
     bot.sendMessage(119405630, telegram_text, parse_mode="HTML")
-    
+
+
 def create_supplier(
     supplier_name,
     supplier_street,
@@ -158,27 +192,69 @@ def create_supplier(
     supplier_zip,
     supplier_phone,
     supplier_email,
-): 
-    odoo = odoorpc.ODOO('host.docker.internal', port=8069)  
+):
+    odoo = odoorpc.ODOO("host.docker.internal", port=8069)
 
-    db = 'health1'
-    username = 'admin'
-    password = 'admin'
+    db = "health1"
+    username = "admin"
+    password = "admin"
     odoo.login(db, username, password)
-    
-    Partner = odoo.env['res.partner']
 
-    supplier_id = Partner.create({
-        'name': supplier_name,
-        'street': supplier_street,
-        'city': supplier_city,
-        'zip': supplier_zip,
-        'country_id': 109,
-        'phone': supplier_phone,
-        'email': supplier_email,
-        'supplier_rank': 1,
-        'company_type': 'company',
-    })
-    
-    odoo_url = f'http://localhost:8069/web#id={supplier_id}&model=res.partner&view_type=form'
+    Partner = odoo.env["res.partner"]
+
+    supplier_id = Partner.create(
+        {
+            "name": supplier_name,
+            "street": supplier_street,
+            "city": supplier_city,
+            "zip": supplier_zip,
+            "country_id": 109,
+            "phone": supplier_phone,
+            "email": supplier_email,
+            "supplier_rank": 1,
+            "company_type": "company",
+        }
+    )
+
+    odoo_url = (
+        f"http://localhost:8069/web#id={supplier_id}&model=res.partner&view_type=form"
+    )
+    return [True, odoo_url]
+
+
+def create_customer(
+    customer_name,
+    customer_street,
+    customer_city,
+    customer_zip,
+    customer_phone,
+    customer_email,
+    customer_type,
+):
+    odoo = odoorpc.ODOO("host.docker.internal", port=8069)
+
+    db = "health1"
+    username = "admin"
+    password = "admin"
+    odoo.login(db, username, password)
+
+    Partner = odoo.env["res.partner"]
+
+    customer_id = Partner.create(
+        {
+            "name": customer_name,
+            "street": customer_street,
+            "city": customer_city,
+            "zip": customer_zip,
+            "country_id": 109,
+            "phone": customer_phone,
+            "email": customer_email,
+            "customer_rank": 1,
+            "company_type": customer_type,
+        }
+    )
+
+    odoo_url = (
+        f"http://localhost:8069/web#id={customer_id}&model=res.partner&view_type=form"
+    )
     return [True, odoo_url]
